fd3b28f3d3961f2d8126698132eeeb32208f0b1e
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-09-29 15:23:24 +0400

    add java role

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,7 @@ mkdir -p ./provisioning/roles
 echo "get roles"
 ansible-galaxy install bennojoy.nginx,master --roles-path='./provisioning/roles'
 ansible-galaxy install bennojoy.redis,master  --roles-path='./provisioning/roles'
+ansible-galaxy install kunik.java,master  --roles-path='./provisioning/roles'
 ansible-galaxy install kunik.elasticsearch,master  --roles-path='./provisioning/roles'
 ansible-galaxy install zzet.common,master  --roles-path='./provisioning/roles'
 ansible-galaxy install zzet.rbenv,master  --roles-path='./provisioning/roles'
