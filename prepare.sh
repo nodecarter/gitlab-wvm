eec8c504b90d71ff1862b66f0a4db6b696d62f31
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-09-29 13:33:39 +0400

    add runit to prepare.sh, update to new ans syntax

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,13 @@
 echo "mkdir for roles"
 mkdir -p ./provisioning/roles
 echo "get roles"
-ansible-galaxy install bennojoy.nginx --roles-path='./provisioning/roles'
-ansible-galaxy install bennojoy.redis --roles-path='./provisioning/roles'
-ansible-galaxy install kunik.elasticsearch --roles-path='./provisioning/roles'
-ansible-galaxy install zzet.common --roles-path='./provisioning/roles'
-ansible-galaxy install zzet.rbenv --roles-path='./provisioning/roles'
-ansible-galaxy install zzet.postgresql --roles-path='./provisioning/roles'
-ansible-galaxy install zzet.gitlab --roles-path='./provisioning/roles'
+ansible-galaxy install bennojoy.nginx,master --roles-path='./provisioning/roles'
+ansible-galaxy install bennojoy.redis,master  --roles-path='./provisioning/roles'
+ansible-galaxy install kunik.elasticsearch,master  --roles-path='./provisioning/roles'
+ansible-galaxy install zzet.common,master  --roles-path='./provisioning/roles'
+ansible-galaxy install zzet.rbenv,master  --roles-path='./provisioning/roles'
+ansible-galaxy install zzet.runit,master  --roles-path='./provisioning/roles'
+ansible-galaxy install zzet.postgresql,master  --roles-path='./provisioning/roles'
+ansible-galaxy install zzet.gitlab,master  --roles-path='./provisioning/roles'
 echo ""
 echo "please, run vagrant up"
